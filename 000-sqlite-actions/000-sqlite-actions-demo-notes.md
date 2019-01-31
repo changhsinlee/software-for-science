404b833c011c915629523081335ba4dc58749c0f
Sean Gallagher <stgallag@gmail.com>
2019-01-30 20:59:21 -0500

    add sqlite actions code

---
+++ 000-sqlite-actions/000-sqlite-actions-demo-notes.md
@@ -0,0 +1,11 @@
+- What is software for science?
+- Teaser - what you can do in how many lines of code
+- What is SQLite
+- What do you need to get started
+- Why do we want to define a udf
+Q1: What are the tradeoffs between running a udf in python vs sql
+  - Especially, if this is in memory
+  - Weather example, saving API calls and not losing data
+Q2: How many lines of python would this save you?
+  - Half the code, no batching
+  - Not much faster, but much less to think about
---
+++ 000-sqlite-actions/000-sqlite-actions-demo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sqlite3, re, json
+conn = sqlite3.connect(":memory:")
+
+# Simple example of a UDF
+conn.create_function(
+    "split",
+    2,
+    lambda pattern, string: json.dumps(re.split(pattern, string)))
+cursor = conn.execute(
+    r"SELECT key, value FROM json_each(split('\W+', 'the cat'));")
+
+
+# File metadata UDF
+def file_size(name):
+    ''' Stub to find a file's size '''
+    return 8282
+def file_perm(name):
+    ''' Stub to find a file's permissions '''
+    return 640
+
+names = conn.execute("SELECT name from tab;").fetchall()
+cursor.executemany(
+    "INSERT INTO file_status(size, perm, name) VALUES (?,?,?);",
+    # This whole list goes into memory before we start
+    [[file_size(name), file_perm(name), name]
+        for name in names
+    ]
+)
+
+conn.create_function("file_size", 1, file_size)
+conn.create_function("file_perm", 1, file_perm)
+cursor.execute(
+    # This is evaluated as we go
+    """INSERT INTO file_status(size, perm, name)
+    SELECT file_size(name), file_perm(name), name
+    FROM tab;"""
+)
